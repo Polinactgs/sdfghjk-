8a3c963a90fdac656ad42fd35563775e89df24c1
Polinactgs <collegevasilenko44@gmail.com>
2024-05-30 10:35:26 +0300

    Add files via upload

---
+++ test4.py
@@ -0,0 +1,5 @@
+#create tuple
+a = (1, 1.1, 'a', [4, 5, 6], {'a':1, 'b':2}, None, True)
+a = ()
+b = tuple()
+b = (1,)
---
+++ test6.py
@@ -0,0 +1,26 @@
+# 1
+is_rainy = True # дождь будет
+
+if is_rainy:
+    print('Беру зонт')
+    print('Надеть сапоги')
+else:
+    print('Не беру зонт')
+
+print('Иду гулять')
+
+# 2
+is_rainy = True # дождя не будет
+
+if is_rainy:
+    print('Беру зонт')
+    print('Надеваю сапоги')
+else:
+    print('Не беру зонт')
+
+print('Иду гулять')
+
+# 3
+is_rainy = True # дождь будет
+
+if is_rainy:
